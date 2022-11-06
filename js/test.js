7367613a04363f6cbbdd76fdeef439a9ab6c7405
[thomas shelby] <92592875+edon99@users.noreply.github.com>
2022-11-06 19:06:19 +0100

    changed tabs n dat

--- js/test.js
+++ js/test.js
@@ -1,93 +1,70 @@
-$( document ).ready(function(){
+// Master DOManipulator v2 ------------------------------------------------------------
+const items = document.querySelectorAll('.item'),
+controls = document.querySelectorAll('.control'),
+headerItems = document.querySelectorAll('.item-header'),
+descriptionItems = document.querySelectorAll('.item-description'),
+activeDelay = .76,
+interval = 5000;
+
+let current = 0;
+
+const slider = {
+  init: () => {
+    controls.forEach(control => control.addEventListener('click', e => {slider.clickedControl(e);}));
+    controls[current].classList.add('active');
+    items[current].classList.add('active');
+  },
+  nextSlide: () => {// Increment current slide and add active class
+    slider.reset();
+    if (current === items.length - 1) current = -1; // Check if current slide is last in array
+    current++;
+    controls[current].classList.add('active');
+    items[current].classList.add('active');
+    slider.transitionDelay(headerItems);
+    slider.transitionDelay(descriptionItems);
+  },
+  clickedControl: e => {// Add active class to clicked control and corresponding slide
+    slider.reset();
+    clearInterval(intervalF);
+
+    const control = e.target,
+    dataIndex = Number(control.dataset.index);
+
+    control.classList.add('active');
+    items.forEach((item, index) => {
+      if (index === dataIndex) {// Add active class to corresponding slide
+        item.classList.add('active');
+      }
+    });
+    current = dataIndex; // Update current slide
+    slider.transitionDelay(headerItems);
+    slider.transitionDelay(descriptionItems);
+    intervalF = setInterval(slider.nextSlide, interval); // Fire that bad boi back up
+  },
+  reset: () => {// Remove active classes
+    items.forEach(item => item.classList.remove('active'));
+    controls.forEach(control => control.classList.remove('active'));
+  },
+  transitionDelay: items => {// Set incrementing css transition-delay for .item-header & .item-description, .vertical-part, b elements
+    let seconds;
+
+    items.forEach(item => {
+      const children = item.childNodes; // .vertical-part(s)
+      let count = 1,
+      delay;
+
+      item.classList.value === 'item-header' ? seconds = .015 : seconds = .007;
+
+      children.forEach(child => {// iterate through .vertical-part(s) and style b element
+        if (child.classList) {
+          item.parentNode.classList.contains('active') ? delay = count * seconds + activeDelay : delay = count * seconds;
+          child.firstElementChild.style.transitionDelay = `${delay}s`; // b element
+          count++;
+        }
+      });
+    });
+  } };
 
 
-
-
-
-
-    // without animation
-    /*
-    $("#close-btn").click(function(){
-       $(".small-image").removeClass('active');
-      $("#modal").hide();
-    })
-  
-  
-    $(".small-image").click(function(){
-       // add active class
-       $(this).addClass('active');
-      var image_path = $(this).attr('src'); 
-      $("#modal").hide();
-      // now st this path to our popup image src
-      $("#modal").show();
-      $("#large-image").attr('src',image_path);
-  
-    })
-  */
-  
-    // with animation  
-  
-      $("#close-btn").click(function(){
-         // remove active class from all images
-        $(".small-image").removeClass('active');
-        $("#modal").slideUp();
-      })
-  
-      $(".small-image").click(function(){
-          // remove active class from all images
-         $(".small-image").removeClass('active');
-        // add active class
-         $(this).addClass('active');
-  
-        var image_path = $(this).attr('src'); 
-        $("#modal").fadeOut();
-        // now st this path to our popup image src
-        $("#modal").fadeIn();
-        $("#large-image").attr('src',image_path);
-  
-      })
-  
-  })
-  
-  
-  // javascript
-  
-  // var small_images  = document.getElementsByClassName("small-image");
-  // var show_image_popup  = document.getElementById("show_image_popup");
-  // var large_image  = document.getElementById("large-image");
-  // var close_btn = document.getElementById("close-btn");
-  
-  // for(var i=0; i< small_images.length; i++){
-  
-  //   small_images[i].addEventListener("click", function(){
-  //     // remove active class from every images
-  //     for(var j=0; j< small_images.length; j++){
-  //       small_images[j].classList.remove('active');
-  //     }
-  //     // end
-   
-  //     this.classList.add('active'); // add active class to this image
-      
-  //     var src_val = this.src;
-  //     large_image.src = src_val;
-  //     showModal();
-  //   });
-  // }
-  
-  
-  // function showModal(){
-  //   show_image_popup.style.display = 'block';
-  // }
-  
-  // close_btn.addEventListener("click", function(){
-  //   // before colose the modal we need to remove active class
-  //   for(var i=0; i< small_images.length; i++){
-  //     small_images[i].classList.remove('active');
-  //   }
-  //   // end
-  //   closeModal();
-  // });
-  
-  // function closeModal(){
-  //   show_image_popup.style.display = 'none';
-  // }
+let intervalF = setInterval(slider.nextSlide, interval);
+slider.init();

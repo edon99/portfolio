e34928dcd5cedbe0981f93b817fa19420c4b414c
edon99 <ofoura07@gmail.com>
2025-04-01 03:02:48 +0100

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,16 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
-import Components from 'unplugin-vue-components/vite';
-import {PrimeVueResolver} from '@primevue/auto-import-resolver';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     tailwindcss(),
-    Components({
-      resolvers: [
-        PrimeVueResolver()
-      ]
-    })]
+    ],
+    alias:{
+      vue: 'vue/dist/vue.esm-bundler.js',
+    },
+  
 })
